26264b72018cc97347cc66e54b42dc208f9092eb
Ernests <ernests.dane@gmail.com>
2023-06-21 00:48:23 +0300

    feat: early version of GET endpoint

---
+++ server/api/firestore/user/[id].js
@@ -0,0 +1,12 @@
+import useFirebaseServer from "../../../utils/firebaseServer"
+
+const { firestore } = useFirebaseServer();
+
+
+export default defineEventHandler(async event => { 
+    const {id} = event.context.params
+    const ref = firestore.doc(`users/${id}`);
+    const snapshot = await ref.get();
+    const data = snapshot.data();
+    return data;
+})
