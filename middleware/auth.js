45dd23d6f6212f994013bbedfbc92a6ffbbcab0f
Ernests <ernests.dane@gmail.com>
2023-06-21 00:48:23 +0300

    Server-side getUser

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@
 
 export default defineNuxtRouteMiddleware( async (to, from) => {
     if(process.server){ //make sure this is only run on the server
-        const cookie = useCookie('authCookie')
+        const cookie = useCookie('authCookie') //cookie is made in login.post.js
         // cookie.value = 'asdf'
         const response = await fetch('http://localhost:3000/api/checkauthstatus', {
             method: 'POST',
