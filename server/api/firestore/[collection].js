abc8987e9b9edc6846d0911759747e5d6fad2f22
Ernests <ernests.dane@gmail.com>
2023-06-25 17:07:41 +0300

    Fixed useFirebaseServer imports in API composables

--- server/api/firestore/[collection].js
+++ server/api/firestore/[collection].js
@@ -1,17 +1,18 @@
-import { firestore } from '~/composables/useFirebaseServer.js'
+import useFirebaseServer from "../../../composables/useFirebaseServer";
 
 // Handles the event of fetching a whole collection from Firestore
 export default defineEventHandler(async (event) => {
-  const { collection: collectionName } = event.context.params;
+    const { firestore } = useFirebaseServer();
+    const { collection: collectionName } = event.context.params;
 
-  // Gets a reference to the specified collection in Firestore
-  const colRef = firestore.collection(collectionName);
+    // Gets a reference to the specified collection in Firestore
+    const colRef = firestore.collection(collectionName);
 
-  // Fetches all documents in the collection
-  const docsSnapshot = await colRef.get();
+    // Fetches all documents in the collection
+    const docsSnapshot = await colRef.get();
 
-  // Transforms the documents data into a more usable format
-  const data = docsSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    // Transforms the documents data into a more usable format
+    const data = docsSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
 
-  return data;
+    return data;
 });
