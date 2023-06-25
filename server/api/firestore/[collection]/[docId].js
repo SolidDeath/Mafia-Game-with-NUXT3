d5121104d97c808fbf132a2e0eebe2619b7b1304
Ernests <ernests.dane@gmail.com>
2023-06-25 17:07:41 +0300

    fix: useFirebaseServer imports in API composables

--- server/api/firestore/[collection]/[docId].js
+++ server/api/firestore/[collection]/[docId].js
@@ -1,20 +1,22 @@
-import { firestore } from '~/composables/useFirebaseServer.js'
+import useFirebaseServer from "../../../composables/useFirebaseServer";
+
 
 // Handles the event of fetching a specific document from a Firestore collection
 export default defineEventHandler(async (event) => {
-  const { collection: collectionName, docId } = event.context.params;
+    const { firestore } = useFirebaseServer();
+    const { collection: collectionName, docId } = event.context.params;
 
-  // Gets a reference to the specified document in Firestore
-  const docRef = firestore.collection(collectionName).doc(docId);
+    // Gets a reference to the specified document in Firestore
+    const docRef = firestore.collection(collectionName).doc(docId);
 
-  // Fetches the document data
-  const docSnapshot = await docRef.get();
+    // Fetches the document data
+    const docSnapshot = await docRef.get();
 
-  // Checks if the document exists and returns its data
-  if (docSnapshot.exists) {
-    let data = { ...docSnapshot.data(), id: docSnapshot.id };
-    return data;
-  } else {
-    throw new Error("Document not found");
-  }
+    // Checks if the document exists and returns its data
+    if (docSnapshot.exists) {
+        let data = { ...docSnapshot.data(), id: docSnapshot.id };
+        return data;
+    } else {
+        throw new Error("Document not found");
+    }
 });
