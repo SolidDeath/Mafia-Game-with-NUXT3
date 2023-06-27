5f8fb28362b774361f6e704af9dfae96d0258d1b
Ernests <ernests.dane@gmail.com>
2023-06-27 12:52:55 +0300

    feat: API put endpoints
    Added and tested

--- composables/addDocServer.ts
+++ composables/addDocServer.ts
@@ -1,44 +1,41 @@
-export default function addDocServer() {
-    async function addDocServe(
-        collectionName: string, 
-        data: object, 
-        docId?: string, 
-        subcollection?: string, 
-        subDocId?: string
-    ){
-        try {
-            let endpoint = 'http://localhost:3000/api/firestore/' + collectionName;
-        
-            if (docId) {
-                endpoint += '/' + docId;
-                if (subcollection) {
-                    endpoint += '/' + subcollection;
-                    if (subDocId) {
-                        endpoint += '/' + subDocId;
-                    } else{
-                        endpoint += '/document';
-                    }
-                }
-            } else{
-                endpoint += '/document';
-            }
-            // console.log("This is the API endpoint: ", endpoint);
-            
-            const response = await $fetch(endpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-        
-            return { data: response, error: null };
-        } catch (error) {
-            return { data: null, error };
-        }
-    };
+export default async function addDocServer(
+    collectionName: string, 
+    data: object, 
+    docId?: string, 
+    subcollection?: string, 
+    subDocId?: string
+){
+    try {
+        let endpoint = 'http://localhost:3000/api/firestore/' + collectionName;
     
-    return { addDocServe }
-}
+        if (docId) {
+            endpoint += '/' + docId;
+            if (subcollection) {
+                endpoint += '/' + subcollection;
+                if (subDocId) {
+                    endpoint += '/' + subDocId;
+                } else{
+                    endpoint += '/document';
+                }
+            }
+        } else{
+            endpoint += '/document';
+        }
+        // console.log("This is the API endpoint: ", endpoint);
+        
+        const response = await $fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    
+        return { data: response, error: null };
+    } catch (error) {
+        return { data: null, error };
+    }
+};
+
 
   
