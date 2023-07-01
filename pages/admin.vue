ec1940938ab06fa9843b486fe8bd08d50fccbdc3
Ernests <ernests.dane@gmail.com>
2023-07-01 23:58:15 +0300

    feat: started work on userList

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,7 +4,7 @@
     <div class="flex w-full items-center justify-center">
       <div class="w-full items-center justify-items-center gap-4 max-w-[550px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_role') }}</button>
-        <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_admin')}}</button>
+        <button class="w-[100px] h-[45px] rounded-md text-center text-accent bg-background text-sm md:text-md" @click="">{{$t('add_admin')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_preset')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('edit_settings')}}</button>
       </div>
---
+++ pages/userList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{ users }}</p>
+    </div>
+</template>
+
+<script setup>
+    const { subscribeCollection, getCollection, updateDocument } = useFirestore()
+
+    const users = ref([])
+    const isPending = ref(false)
+
+    users.value = useFetch("firestore", users)
+
+    onMounted(() => {
+        users.value = subscribeCollection("users")
+    })
+</script>
