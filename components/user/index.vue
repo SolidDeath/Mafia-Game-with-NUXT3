a4f286ea864eab5a3b4424bb86a387cba66dd9e8
Ernests <ernests.dane@gmail.com>
2023-07-04 13:50:04 +0200

    feat: added custom elements

--- components/user/index.vue
+++ components/user/index.vue
@@ -2,26 +2,34 @@
     <div class="card">
         <div class="card-body">
             <div class="flex flex-col items-center">
-                <div class="flex flex-col items-center">
+                    <!-- TODO: style error-->
+                <h2 v-if="showError">{{ showError }}</h2>
+                <form class="flex flex-col items-center">
                     <img :src="userIcon" class="w-24 h-24 rounded-full" />
                     <h1 class="text-2xl font-bold mt-2">{{ displayName }}</h1>
                     <div>
-                        <label for="email">{{ $t("email") }}</label>
-                        <InputWithCopy :value="email" name="email"/>
+                        <label for="emailInput">{{ $t("email") }}</label>
+                        <InputWithCopy :value="email" name="emailInput"/>
                     </div>
                     <div>
-                        <label for="email">{{ $t("user_id") }}</label>
-                        <InputWithCopy :value="userId" name="userId"/>
+                        <label for="userIdInput">{{ $t("user_id") }}</label>
+                        <InputWithCopy :value="userId" name="userIdInput"/>
                     </div>
                     <div>
-                        <label for="userTypes">{{ $t("access_level") }}: </label>
+                        <label for="accessLevelDrop">{{ $t("access_level") }}: </label>
+                        <!-- TODO: fix props or emits, does not update properly -->
                         <Dropdown 
                             :options="userTypes" 
                             v-model="userForm.userAccessLevel"
-                            name="userTypes"
+                            name="accessLevelDrop"
+                            @update:modelValue="(val) => userForm.userAccessLevel = val"
                         />
                     </div>
-                </div>
+                    <div class="text-center">
+                        <!-- TODO: Add dynamic buttons -->
+                        <Button class="bg-blue-500 text-white w-56" type="submit" @click.prevent="handleSave">{{$t("save")}}</Button>
+                    </div>
+                </form>
       
             </div>
         </div>
@@ -29,33 +37,58 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  displayName: String,
-  email: String,
-  accessLevel: Number,
-  userIcon: String,
-  userId: String,
-  modelValue: Number
-})
 
-const userForm = reactive({
-    userAccessLevel: 0
-})
+    const { updateDocument } = useFirestore()
+    const showError = ref("")
+    const isPending = ref(false)
+    const props = defineProps({
+        displayName: String,
+        email: String,
+        accessLevel: Number,
+        userIcon: String,
+        userId: String,
+        modelValue: Number
+    })
 
-const userTypes = ref([
-    { text: '0 - Banned user', value: 0 },
-    { text: '1 - User with an account', value: 1 },
-    { text: '2 - Premium user', value: 2 },
-    { text: '3 - Admin', value: 3 },
-    { text: '4 - Super admin', value: 4 },
-]);
+    const userForm = reactive({
+        userAccessLevel: 0
+    })
 
-function checkAccessLevel(input) {
-  return /^[1-4]$/.test(input);
-}
+    const userTypes = ref([
+        { text: '0 - Banned user', value: 0 },
+        { text: '1 - User with an account', value: 1 },
+        { text: '2 - Premium user', value: 2 },
+        { text: '3 - Admin', value: 3 },
+        { text: '4 - Super admin', value: 4 },
+    ]);
 
-onMounted(() => {
-    userForm.userAccessLevel = props.accessLevel
-})
+    function checkAccessLevelRange(input) {
+        return /^[0-4]$/.test(input);
+    }
+
+    onMounted(() => {
+        userForm.userAccessLevel = props.accessLevel
+    })
+
+    const saveUpdates = async () => {
+        console.log(userForm);
+        await updateDocument("users", props.userId, {
+            accessLevel: userForm.userAccessLevel
+        })
+    }
+
+    const handleSave = () => {
+        console.log('saving');
+        isPending.value = true
+        if(checkAccessLevelRange(userForm.userAccessLevel)){
+            console.log('saving');
+            saveUpdates()
+            // navigateTo(localPath('/dashboard'))
+        }
+        else{
+            showError.value = 'Username can only contain letters (a-Z)'
+        }
+        isPending.value = false
+    }
 
 </script>
