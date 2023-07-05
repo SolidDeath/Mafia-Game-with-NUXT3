99d25bbc50138553e61a003c8dda2f5d059e01f3
Ernests <ernests.dane@gmail.com>
2023-07-05 23:38:01 +0200

    refactror: Sorted pages into folders

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -4,7 +4,7 @@
     <div class="flex w-full items-center justify-center">
       <div class="w-full items-center justify-items-center gap-4 max-w-[550px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_role') }}</button>
-        <button class="w-[100px] h-[45px] rounded-md text-center text-accent bg-background text-sm md:text-md" @click="handleUsers">{{$t('add_admin')}}</button>
+        <button class="w-[100px] h-[45px] rounded-md text-center text-accent bg-background text-sm md:text-md" @click="handleUsers">{{$t('user_list')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_preset')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('edit_settings')}}</button>
       </div>
@@ -24,6 +24,6 @@ definePageMeta({
 const localPath = useLocalePath()
 
 const handleUsers = () => {
-  navigateTo(localPath('/userList'))
+  navigateTo(localPath('/admin/userList'))
 }
 </script>
