eff02ec1fc5f154eeec87be80c04a4236e314eaa
Ernests <ernests.dane@gmail.com>
2023-07-06 17:40:15 +0200

    feat: Added addRole view, no content

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center font-Ax font-bold text-accent text-md md:text-lg lg:text-xl text-Ax0 mb-2 pb-8">{{ $t('admin_panel') }}</h1>
     <div class="flex w-full items-center justify-center">
       <div class="w-full items-center justify-items-center gap-4 max-w-[550px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_role') }}</button>
+        <button class="w-[100px] h-[45px] rounded-md text-center text-accent bg-background text-sm md:text-md" @click="handleAddRole">{{$t('add_role') }}</button>
         <button class="w-[100px] h-[45px] rounded-md text-center text-accent bg-background text-sm md:text-md" @click="handleUsers">{{$t('user_list')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('add_preset')}}</button>
         <button class="w-[100px] h-[45px] hover:cursor-not-allowed rounded-md text-center text-accent bg-background text-sm md:text-md" disabled>{{$t('edit_settings')}}</button>
@@ -25,5 +25,9 @@ const localPath = useLocalePath()
 
 const handleUsers = () => {
   navigateTo(localPath('/admin/userList'))
+}
+
+const handleAddRole = () => {
+  navigateTo(localPath('/admin/addRole'))
 }
 </script>
