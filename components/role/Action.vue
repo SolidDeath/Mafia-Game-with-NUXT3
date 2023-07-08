fcbbf94e10d048e54076234205763147d5b9618f
Ernests <ernests.dane@gmail.com>
2023-07-08 23:45:54 +0200

    feat: AAction component

---
+++ components/role/Action.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="action-row">
+        <img :src="iconUrl" alt="Action Icon" class="action-icon" v-if="iconUrl" />
+        <FormGroup :label="$t('role_action_title')" @input="$emit('update:actionTitle', $event.target.value)" type="text" :value="props.actionTitle"/>
+        <FormGroup :label="$t('role_action_immediate')" @input="$emit('update:immediate', $event.target.value)" type="checkbox" :value="props.immediate"/>
+        <FormGroup :label="$t('role_action_immediateActionField')" @input="$emit('update:immediateActionField', $event.target.value)" type="text" :value="props.immediateActionField" v-if="props.immediate == true"/>
+        <FormGroup :label="$t('role_action_isNightAction')" @input:="$emit('update:isNightAction', !props.isNightAction)" type="checkbox" :value="props.isNightAction"/>
+        <UploadIcon @update:iconUrl="$emit('update:iconUrl', $event)" header="Action icon" collection="actionIcons"/>
+        <Button class="material_icons" @click="$emit('remove')">minus</Button>
+    
+    </div>
+  </template>
+  
+  <script setup>
+  /*
+    PROPS
+  */
+    const props = defineProps({
+        actionTitle: {
+            type: String,
+            default: ''
+        },
+        iconUrl: {
+            type: String,
+            default: ''
+        },
+        isNightAction: {
+            type: Boolean,
+            default: true
+        },
+        immediate: {
+            type: Boolean,
+            default: false
+        },
+        immediateActionField: {
+            type: String,
+            default: ''
+        }
+    })
+
+
+    defineEmits([
+        'update:actionTitle',
+        'update:iconUrl',
+        'update:isNightAction',
+        'update:immediate',
+        'update:immediateActionField',
+        'remove'
+    ])
+  </script>
+  
+  <style scoped>
+  .action-row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+  }
+  
+  .action-icon {
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+  }
+  
+  .action-title, .action-night, .action-immediate {
+    margin-right: 10px;
+  }
+  </style>
+  
--- pages/role/Form.vue
+++ pages/role/Form.vue
@@ -21,11 +21,44 @@
         </div>
 
         <FormGroup :label="$t('can_be_killed_by_mafia')" v-model="roleInfo.CAN_BE_KILLED_BY_MAFIA" type="checkbox"/>
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="checkbox"/>
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="checkbox"/>
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="checkbox"/>
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="checkbox"/>
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="checkbox"/>
+        <FormGroup :label="$t('can_be_multiple')" v-model="roleInfo.CAN_BE_MULTIPLE" type="checkbox"/>
+        
+    
+        <div>
+            <label for="investigation_responses">{{ $t('investigation_responses') }}</label>
+            <div name="investigation_responses">
+                <div>
+                    <label for="investigation_res_aura">{{ $t('investigation_res_aura') }}</label>
+                    <Dropdown :options="auraList"  v-model="roleInfo.INVESTIGATION_RES.AURA" name="investigation_res_aura"/>
+                </div>
+                <div>
+                    <label for="investigation_res_alignment">{{ $t('investigation_res_alignment') }}</label>
+                    <Dropdown :options="alignmentList"  v-model="roleInfo.INVESTIGATION_RES.ALIGNMENT" name="investigation_res_alignment"/>
+                </div>
+            </div>  
+        </div>
+
+        
+        <!-- ACTIONS -->
+        <div class="actions">
+          <RoleAction
+                v-for="(action, index) in actionArr"
+            
+                :actionTitle="action.ACTION_TITLE"
+                :immediate="action.IMMEDIATE"
+                :immediateActionField="action.IMMEDIATE_ACTION_FIELD"
+                :isNightAction="action.IS_NIGHT_ACTION"
+                @update:isNightAction="value => action.IS_NIGHT_ACTION = value"
+                @update:immediateActionField="value => action.IMMEDIATE_ACTION_FIELD = value"
+                @update:immediate="value => action.IMMEDIATE = value"
+                @update:actionTitle="value => action.ACTION_TITLE = value"
+                @remove="() => actionArr.splice(index, 1)"
+
+            />
+            <p>{{ actionArr }}</p>
+            <Button @click="addAction">{{ $t('add_action') }}</Button>
+
+        </div>
     </form>
 </template>
 
@@ -41,44 +74,60 @@
         IMPORTS    
     */
         const { ALIGNMENT: alignmentList, AURA: auraList, WIN_CONDITION, CATEGORY: categoryList, ACTION: actionList} = useDropdowns()
-        const { subscribeCollection, getDocument, updateDocument } = useFirestore()
+        const { subscribeCollection, getDocument, updateDocument,subscribeDocument } = useFirestore()
 
     /*
         GLOBAL VARIABLES
     */
 
-        const roleDoc = ref(null)
-        
-        const roleInfo = reactive({
-            ROLE_NAME: '',
-            ROLE_DESCRIPTION: '',
-            AURA: '',
-            ALIGNMENT: '',
-            CATEGORY: '',
-            WIN_CONDITION: '',
-            CAN_BE_KILLED_BY_MAFIA: false,
-            HAS_NIGHT_ACTION: false,
-            HAS_DAY_ACTION: false,
-            CAN_BE_MULTIPLE: false,
-            INVESTIGATION_RES: {
+        const roleDoc = ref()
+        const actionArr = ref([])
+
+        const roleInfo = reactive(
+            {
+                ROLE_NAME: '',
+                ROLE_DESCRIPTION: '',
                 AURA: '',
                 ALIGNMENT: '',
-            },
-            ICON_URL: ''
-        })
+                CATEGORY: '',
+                WIN_CONDITION: '',                
+                HAS_NIGHT_ACTION: false,
+                HAS_DAY_ACTION: false,
+                CAN_BE_KILLED_BY_MAFIA: false,
+                CAN_BE_MULTIPLE: false,
+                INVESTIGATION_RES: {
+                    AURA: '',
+                    ALIGNMENT: ''
+                },
+                ICON_URL: '',
+                ACTIONS: []
+            }
+        )
      
         const action = {
             ACTION_TITLE: '',
             ICON_URL: '',
+            IS_NIGHT_ACTION: true,
             IMMEDIATE: false,
             IMMEDIATE_ACTION_FIELD: '' //The field that is toggled if the action is immediate
         }
 
+        /*
+            COMPUTED ROLE VALUES
+        */
+            roleInfo.HAS_NIGHT_ACTION = computed(() => {
+                return roleInfo.ACTIONS.filter(obj => obj.IS_NIGHT_ACTION === true).length > 0 ? true : false;
+            })
+
+            roleInfo.HAS_DAY_ACTION = computed(() => {
+                return roleInfo.ACTIONS.filter(obj => obj.IS_NIGHT_ACTION === false).length > 0 ? true : false;
+            })
     /*
         SSR
     */
-        if(process.server && roleID){
-            roleDoc = await useFetch('../api/roles/' + props.roleID)
+        if(process.server && props.roleID){
+            roleDoc.value = await useFetch('../api/roles/' + props.roleID)
+            console.log(roleDoc.value);
         }
         
         watchEffect(() => {
@@ -88,8 +137,16 @@
             }
         })
 
+        onMounted(async () => {
+            // if(props.roleID){
+            //     roleDoc.value = subscribeDocument('roles', props.roleID)
+            // }
+        })
+
+        const addAction = () => {
+            actionArr.value.push(action)
+        }
 
 
-    const actions = ref([])
 
 </script>
