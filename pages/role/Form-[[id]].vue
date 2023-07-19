c9adaf8a4d2bce958c80e697c89c5d54888de618
Ernests <ernests.dane@gmail.com>
2023-07-19 20:58:47 +0300

    fix: Fixed RoleAction parameters

--- pages/role/Form-[[id]].vue
+++ pages/role/Form-[[id]].vue
@@ -1,28 +1,28 @@
 <template>
-    <p> {{ roleDoc }}</p>
-    <form v-bind="$attrs" @submit.prevent="processForm">
-        <FormGroup :label="$t('role_name')" v-model="roleInfo.ROLE_NAME" type="text"/>
-        <FormGroup :label="$t('role_description')" v-model="roleInfo.ROLE_DESCRIPTION" type="text"/>
+    <p>{{ roleDoc }}</p>
+    <form v-bind="$attrs" @submit.prevent="processForm" >
+        <FormGroup :label="$t('role_name')" v-model="roleName" type="text"/>
+        <FormGroup :label="$t('role_description')" v-model="roleDescription" type="text"/>
 
         <div>
             <label for="role_aura">{{ $t('role_aura') }}</label>
-            <Dropdown name="role_aura" :options="auraList"  v-model="roleInfo.AURA"/>
+            <Dropdown name="role_aura" :options="auraList"  v-model="aura"/>
         </div>
         <div>
             <label for="role_alignment">{{ $t('role_alignment') }}</label>
-            <Dropdown :options="alignmentList"  v-model="roleInfo.ALIGNMENT" name="role_alignment"/>
+            <Dropdown :options="alignmentList"  v-model="alignment" name="role_alignment"/>
         </div>
         <div>
             <label for="role_category">{{ $t('role_category') }}</label>
-            <Dropdown :options="categoryList"  v-model="roleInfo.CATEGORY" name="role_category"/>
+            <Dropdown :options="categoryList"  v-model="category" name="role_category"/>
         </div>
         <div>
             <label for="role_winCondition">{{ $t('role_winCondition') }}</label>
-            <Dropdown :options="WIN_CONDITION"  v-model="roleInfo.WIN_CONDITION" name="role_winCondition"/>
+            <Dropdown :options="WIN_CONDITION"  v-model="winCondition" name="role_winCondition"/>
         </div>
 
-        <FormGroup :label="$t('can_be_killed_by_mafia')" v-model="roleInfo.CAN_BE_KILLED_BY_MAFIA" type="checkbox"/>
-        <FormGroup :label="$t('can_be_multiple')" v-model="roleInfo.CAN_BE_MULTIPLE" type="checkbox"/>
+        <FormGroup :label="$t('can_be_killed_by_mafia')" v-model="canBeKilledByMafia" type="checkbox"/>
+        <FormGroup :label="$t('can_be_multiple')" v-model="canBeMultiple" type="checkbox"/>
         
     
         <div>
@@ -30,11 +30,11 @@
             <div name="investigation_responses">
                 <div>
                     <label for="investigation_res_aura">{{ $t('investigation_res_aura') }}</label>
-                    <Dropdown :options="auraList"  v-model="roleInfo.INVESTIGATION_RES.AURA" name="investigation_res_aura"/>
+                    <Dropdown :options="auraList"  v-model="investigationResAura" name="investigation_res_aura"/>
                 </div>
                 <div>
                     <label for="investigation_res_alignment">{{ $t('investigation_res_alignment') }}</label>
-                    <Dropdown :options="alignmentList"  v-model="roleInfo.INVESTIGATION_RES.ALIGNMENT" name="investigation_res_alignment"/> 
+                    <Dropdown :options="alignmentList"  v-model="investigationResAlignment" name="investigation_res_alignment"/> 
                 </div>
             </div>  
         </div>
@@ -45,68 +45,74 @@
           <RoleAction
                 v-for="(action, index) in actionArr" 
                 :key="index"
-            
-                :actionTitle="actionArr[index].ACTION_TITLE"
-                :immediate="actionArr[index].IMMEDIATE"
-                :immediateActionField="actionArr[index].IMMEDIATE_ACTION_FIELD"
-                :isNightAction="actionArr[index].IS_NIGHT_ACTION"
+                
                 v-model:isNightAction="actionArr[index].IS_NIGHT_ACTION"
                 v-model:immediateActionField="actionArr[index].IMMEDIATE_ACTION_FIELD"
                 v-model:immediate="actionArr[index].IMMEDIATE"
                 v-model:actionTitle="actionArr[index].ACTION_TITLE"
                 @remove="() => actionArr.splice(index, 1)"
 
-            />
-            <p>{{ actionArr }}</p>
-            <Button @click="addAction">{{ $t('add_action') }}</Button>
-
-        </div>
-        <Button>{{ $t("save") }}</Button>
-    </form>
+                />
+                <p>{{ actionArr }}</p>
+                <Button @click="addAction">{{ $t('add_action') }}</Button>
+                
+            </div>
+            <Button>{{ $t("save") }}</Button>
+        </form>
 </template>
 
-<script setup>
+<script setup lang="ts">
+
     /*
         PROPS
     */
-
         const props = useRoute().params
-
-
+   
+   
     /*
         IMPORTS    
     */
         const { ALIGNMENT: alignmentList, AURA: auraList, WIN_CONDITION, CATEGORY: categoryList, ACTION: actionList} = useDropdowns()
-        const { subscribeCollection, getDocument, updateDocument,subscribeDocument, getCollection } = useFirestore()
-
+        const { subscribeCollection, getDocument, updateDocument,subscribeDocument, getCollection, addDocument } = useFirestore()
+  
+  
+    /*
+        TYPES
+    */
+ 
+        interface RoleDocType {
+            ROLE_NAME: string;
+            ROLE_DESCRIPTION: string;
+            AURA: string;
+            ALIGNMENT: string;
+            CATEGORY: string;
+            WIN_CONDITION: string;
+            HAS_NIGHT_ACTION: boolean;
+            HAS_DAY_ACTION: boolean;
+            CAN_BE_KILLED_BY_MAFIA: boolean;
+            CAN_BE_MULTIPLE: boolean;
+            INVESTIGATION_RES: {
+                AURA: string;
+                ALIGNMENT: string;
+            };
+            ICON_URL: string;
+        }
+    
     /*
         GLOBAL VARIABLES
     */
-
-        const roleDoc = ref()
+   
+        const immutableActions = ref([])
         const actionArr = ref([])
-
-        const roleInfo = reactive(
-            {
-                ROLE_NAME: '',
-                ROLE_DESCRIPTION: '',
-                AURA: '',
-                ALIGNMENT: '',
-                CATEGORY: '',
-                WIN_CONDITION: '',                
-                HAS_NIGHT_ACTION: false,
-                HAS_DAY_ACTION: false,
-                CAN_BE_KILLED_BY_MAFIA: false,
-                CAN_BE_MULTIPLE: false,
-                INVESTIGATION_RES: {
-                    AURA: '',
-                    ALIGNMENT: ''
-                },
-                ICON_URL: '',
-                ACTIONS: []
-            }
-        )
-     
+        let roleDoc = ref<RoleDocType | null>(null)
+        // if(process.server){
+        //     let roleDoc = await useFetch("../api/firestore/roles/" + props.id)
+        //     console.log("Roledoc: ",roleDoc);
+            
+        // }
+    
+        
+        
         const action = {
             ACTION_TITLE: '',
             ICON_URL: '',
@@ -114,40 +120,133 @@
             IMMEDIATE: false,
             IMMEDIATE_ACTION_FIELD: '' //The field that is toggled if the action is immediate
         }
+        
+   
+    /*
+        COMPUTED PROPERTIES
+    */
+        const roleName = computed({
+           get: () => roleDoc.value?.ROLE_NAME,
+           set: (val) => { if(roleDoc.value) roleDoc.value.ROLE_NAME = val }
+        });
 
-        /*
-            COMPUTED ROLE VALUES
-        */
-            roleInfo.HAS_NIGHT_ACTION = computed(() => {
-                return roleInfo.ACTIONS.filter(obj => obj.IS_NIGHT_ACTION === true).length > 0 ? true : false;
-            })
+        const roleDescription = computed({
+            get: () => roleDoc.value?.ROLE_DESCRIPTION,
+            set: (val) => { if(roleDoc.value) roleDoc.value.ROLE_DESCRIPTION = val }
+        });
 
-            roleInfo.HAS_DAY_ACTION = computed(() => {
-                return roleInfo.ACTIONS.filter(obj => obj.IS_NIGHT_ACTION === false).length > 0 ? true : false;
-            })
+        const aura = computed({
+            get: () => roleDoc.value?.AURA,
+            set: (val) => { if(roleDoc.value) roleDoc.value.AURA = val }
+        });
+
+        const alignment = computed({
+            get: () => roleDoc.value?.ALIGNMENT,
+            set: (val) => { if(roleDoc.value) roleDoc.value.ALIGNMENT = val }
+        });
+
+        const category = computed({
+            get: () => roleDoc.value?.CATEGORY,
+            set: (val) => { if(roleDoc.value) roleDoc.value.CATEGORY = val }
+        });
+
+        const winCondition = computed({
+            get: () => roleDoc.value?.WIN_CONDITION,
+            set: (val) => { if(roleDoc.value) roleDoc.value.WIN_CONDITION = val }
+        });
+
+        const hasNightAction = computed({
+            get: () => roleDoc.value?.HAS_NIGHT_ACTION,
+            set: (val) => { if(roleDoc.value) roleDoc.value.HAS_NIGHT_ACTION = val }
+        });
+
+        const hasDayAction = computed({
+            get: () => roleDoc.value?.HAS_DAY_ACTION,
+            set: (val) => { if(roleDoc.value) roleDoc.value.HAS_DAY_ACTION = val }
+        });
+
+        const canBeKilledByMafia = computed({
+            get: () => roleDoc.value?.CAN_BE_KILLED_BY_MAFIA,
+            set: (val) => { if(roleDoc.value) roleDoc.value.CAN_BE_KILLED_BY_MAFIA = val }
+        });
+
+        const canBeMultiple = computed({
+            get: () => roleDoc.value?.CAN_BE_MULTIPLE,
+            set: (val) => { if(roleDoc.value) roleDoc.value.CAN_BE_MULTIPLE = val }
+        });
+
+        const investigationResAura = computed({
+            get: () => roleDoc.value?.INVESTIGATION_RES?.AURA,
+            set: (val) => { if(roleDoc.value?.INVESTIGATION_RES) roleDoc.value.INVESTIGATION_RES.AURA = val }
+        });
+
+        const investigationResAlignment = computed({
+            get: () => roleDoc.value?.INVESTIGATION_RES?.ALIGNMENT,
+            set: (val) => { if(roleDoc.value?.INVESTIGATION_RES) roleDoc.value.INVESTIGATION_RES.ALIGNMENT = val }
+        });
+
+        const iconURL = computed({
+            get: () => roleDoc.value?.ICON_URL,
+            set: (val) => { if(roleDoc.value) roleDoc.value.ICON_URL = val }
+        });
+
+        watchEffect(() => {
+            console.log(roleName.value);
+            console.log(canBeKilledByMafia.value);
+            console.log(aura.value);
+            console.log(alignment.value);
+            console.log(category.value);
+            console.log(winCondition.value);
+            console.log(hasNightAction.value);
+            console.log(hasDayAction.value);
+            console.log(canBeMultiple.value);
+            console.log(investigationResAura.value);
+            console.log(investigationResAlignment.value);
+            console.log(iconURL.value);
+
+            
+            
+            
+        })
+
+    /*
+        COMPUTED ROLE VALUES
+    */
+            // roleDoc.value.HAS_NIGHT_ACTION = computed(() => {
+            //     let res = actionArr.value.filter(obj => obj.IS_NIGHT_ACTION === true).length > 0 ? true : false;
+            //     console.log(res);
+                
+            //     return res
+            // }).value
+
+            // roleDoc.value.HAS_DAY_ACTION = computed(() => {
+            //     return actionArr.value.filter(obj => obj.IS_NIGHT_ACTION === false).length > 0 ? true : false;
+            // }).value
     /*
         SSR
     */
-        // if(process.server && props.roleID){
-        //     roleDoc.value = await useFetch('../api/roles/' + props.roleID)
-        //     console.log(roleDoc.value);
-        // }
-        
-        watchEffect(() => {
-            if(roleDoc.data){
-                //TODO: populate all of the fields of roleInfo with data from firebase
 
-            }
-        })
+
+
 
         onMounted(async () => {
             console.log('roleID', props.id	);
             if(props.id){
                 try{
-                    roleDoc.value = subscribeDocument('roles', props.id)
-                    console.log(roleDoc.value);
-                    roleInfo.ACTIONS = subscribeCollection('roles', props.id, 'actions')
-                    console.log(roleInfo.ACTIONS);
+                    roleDoc.value = await subscribeDocument<RoleDocType>('roles', props.id)
+                    console.log('roleDoc', roleDoc.value);
+                    
+                    console.log('Aura List:', auraList);
+    
+
+
+
+
+                    // roleDoc.ACTIONS = await subscribeCollection('roles', props.id, 'actions')
+                    
+                    // console.log(roleDoc.ACTIONS);
+
+
 
                 }catch(err){
                     console.log(err);
@@ -165,7 +264,7 @@
             //             actionArr.value.push(actionCopy1);
                         
             //             let actionCopy2 = JSON.parse(JSON.stringify(action));
-            //             roleInfo.ACTIONS.push(actionCopy2);
+            //             roleDoc.ACTIONS.push(actionCopy2);
             //         }
             //     }
                 
@@ -184,7 +283,9 @@
         }
 
         const processForm = async () => {
+            // await addDocument('roles', roleDoc)
             //figure out which actions have changed and update those, delete the ones that are missing and add the new ones
         }
 
 </script>
+
