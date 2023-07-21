9af188f2cee660a4e847525cddcc7d35fbdc867d
Ernests <ernests.dane@gmail.com>
2023-07-21 17:04:11 +0300

    feat: Added email, password and username valiator

---
+++ composables/useValidators.ts
@@ -0,0 +1,32 @@
+import { z, ZodError, ZodParsedType } from 'zod';
+
+export function useValidators(){
+  return {validateEmail, validatePassword, validateUsername}
+}
+
+function validateEmail(email: string) {
+  const emailSchema = z.string().email();
+  try {
+    return { flag: true, email: emailSchema.parse(email) };
+  } catch (err) {
+    return { flag: false, email: (err instanceof ZodError) ? err.issues[0].message : undefined };
+  }
+}
+
+function validatePassword(password: string) {
+  const passwordSchema = z.string().min(6);
+  try {
+    return { flag: true, password: passwordSchema.parse(password) };
+  } catch (err) {
+    return { flag: false, password: (err instanceof ZodError) ? err.issues[0].message : undefined };
+  }
+}
+
+function validateUsername(name: string) {
+  const usernameSchema = z.string().min(3);
+  try {
+    return { flag: true, name: usernameSchema.parse(name) };
+  } catch (err) {
+    return { flag: false, name: (err instanceof ZodError) ? err.issues[0].message : undefined };
+  }
+}
