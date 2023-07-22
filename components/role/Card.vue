dee6c139c591050bbc61db17b3b150d912cc78fe
Ernests <ernests.dane@gmail.com>
2023-07-22 13:41:01 +0300

    fix: Role card size

--- components/role/Card.vue
+++ components/role/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-background box-border cursor-pointer w-48 p-4 h-56 hover:transform hover:translate-y-[-4px] hover:border-b-[4px] border-deep transition-200 transition-all">
+  <div class="bg-background box-border cursor-pointer w-36 h-44 sm:w-42 sm:h-45 p-4 hover:transform hover:translate-y-[-4px] hover:border-b-[4px] border-deep transition-200 transition-all rounded-xl">
       <div class="w-full aspect-square p-4">
         <img :src="iconLink" :alt="name"/>
       </div>
