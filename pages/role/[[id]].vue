a1c08ca04cad71b94b5d4e93a4718bedff807929
Ernests <ernests.dane@gmail.com>
2023-07-31 23:44:30 +0300

    style: Renamed components

--- pages/role/[[id]].vue
+++ pages/role/[[id]].vue
@@ -118,14 +118,15 @@
     /*
         Getting values from the server 
     */
-        let serverRole = await useFetch("../../server/api/firestore/roles/" + props.id)
-        console.log(serverRole.data);
+        let serverRole = await useFetch("/api/firestore/roles/" + props.id)
+        console.log('This is the server role: ',serverRole);
         
-        let serverActions = await useFetch('../api/firestroe/roles/' + props.id + "actions")
-        // console.log('ServerRole', serverRole);
+        let serverActions = await useFetch('../api/firestore/roles/' + props.id + "actions")
+        console.log('ServerRole', serverRole);
+        console.log('ServerActions',serverActions.data);
         
         roleDoc.value = {...serverRole}
-        console.log(roleDoc);
+        // console.log(roleDoc)
         
 
 
