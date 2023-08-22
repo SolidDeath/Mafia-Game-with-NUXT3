0a8aa6a2b69e2ba462c1cb3a401043210712e8dc
Ernests <ernests.dane@gmail.com>
2023-08-22 12:38:50 +0300

    feat: Added support for checkboxes that correctly save boolean

--- components/FormGroup.vue
+++ components/FormGroup.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="flex flex-col space-y-2">
         <Label>{{label}}</Label>
-        <Input :type="type" :value="modelValue" :placeholder="placeholder"
-        @update:modelValue="(val) => $emit('update:modelValue', val)"/>
+        <Input v-if="type !== 'checkbox'" :type="type" :value="modelValue" :placeholder="placeholder"
+               @input="$emit('update:modelValue', $event.target.value)"/>
+        <Input v-else :type="type" :checked="modelValue" :placeholder="placeholder"
+               @change="$emit('update:modelValue', $event.target.checked)"/>
         <p v-if="errorMessage" class="text-red-500 text-sm">{{errorMessage}}</p>
     </div>
 </template>
@@ -12,12 +14,9 @@ defineProps({
     label: String,
     type: String,
     placeholder: String,
-    value: String | Number,
-    modelValue: String | Number,
+    modelValue: String | Number | Boolean,
     errorMessage: String
 })
 
 defineEmits(['update:modelValue'])
-
 </script>
-
