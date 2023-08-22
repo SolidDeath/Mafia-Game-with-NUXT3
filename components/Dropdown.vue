65ca22d88d91bcf930e766d88f2324e55ccee2d2
Ernests <ernests.dane@gmail.com>
2023-08-22 12:38:13 +0300

    fix: Incorrectly implemented modelValue properties

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <select v-bind="$attrs" class="w-full bg-background border border-deep rounded outline-none focus:ring focus:ring-crimson text-gray-400 transition-all duration-150 ease-in-out p-2" @input="$emit('update:currentValue', $event.target.value)" :value="props.currentValue">
+    <select v-bind="$attrs" class="w-full bg-background border border-deep rounded outline-none focus:ring focus:ring-crimson text-gray-400 transition-all duration-150 ease-in-out p-2" @change="$emit('update:modelValue', $event.target.value)" :value="props.modelValue">
       <option v-for="option in options" :key="option.value" :value="option.value" >
         {{ option.name }}
       </option>
@@ -8,13 +8,13 @@
   
   <script setup>
   const props = defineProps({
-    currentValue: Number | String | Boolean,
+    modelValue: Number | String | Boolean,
     options: {
       type: Array,
       required: true
     },
   }) 
-  defineEmits(['update:currentValue'])
+  defineEmits(['update:modelValue'])
   
   </script>
   
